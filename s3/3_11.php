c8628f217876f1335f614b4462314769215b36c2
lmz2932 <lumeizhuo@gmail.com>
2016-01-07 19:14:31 +0800

    null

---
+++ s3/3_11.php
@@ -0,0 +1,15 @@
+<?php
+    var_dump($_COOKIE);
+    echo "<br />";
+
+    var_dump($_SESSION);
+    echo "<br />";
+
+    var_dump($_POST);
+    echo "<br />";
+
+    var_dump($_GET);
+    echo "<br />";
+
+    var_dump($GLOBALS);
+    echo "<br />";
