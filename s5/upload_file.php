2a4b74f7fb9c4c63cec495ba889b21e9d6d43a9c
lmz2932 <lumeizhuo@gmail.com>
2016-01-11 14:02:02 +0800

    modify upload file demo

--- s5/upload_file.php
+++ s5/upload_file.php
@@ -1,19 +1,18 @@
 <?php
     header('content-type:text/html; charset=utf-8');
 
-    function check_uploaded_file($res){
-        if ($res){
-            echo "上传文件成功！<br />";
+    foreach($_FILES as $filename => $arr){
+        if ($arr['error'] == 0){
+            # 保存上传文件的路径(及其上级目录)应该有可写权限
+            if ($arr['size'] > 0){
+                $res = move_uploaded_file($arr['tmp_name'], './uploaded/'.$arr['name']);
+                if ($res){
+                    echo "<script>alert('文件上传成功！');window.location.href='./upload_file.html';</script>"; 
+                }else{
+                    echo "<script>alert('文件上传失败！');history.back();</script>"; 
+                }
+            }
         }else{
             echo "<script>alert('文件上传失败！');history.back();</script>"; 
         }
-    }
-    var_dump($_FILES);
-    foreach($_FILES as $filename => $arr){
-        var_dump($arr['error']);
-        if ($arr['error'] == 0){
-            # 保存上传文件的路径(及其上级目录)应该有可写权限
-            $res = move_uploaded_file($arr['tmp_name'], './uploaded/'.$arr['name']);
-        }
-        check_uploaded_file(!$arr['error'] && $res);
     }
